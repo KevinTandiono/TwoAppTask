f0db34891bb90900c92966604cae371bc2a7323b
Kevin Tandiono <kevintandionoofcl@gmail.com>
2019-11-05 17:43:02 +0800

    1) Added custom action bar 2) Added overall layout for main activity with buttons 3) Added layout for fragment hosting webview 4) Added string resources for texts

--- app/src/main/java/com/aezphel/android/twoappassessment/MainActivity.java
+++ app/src/main/java/com/aezphel/android/twoappassessment/MainActivity.java
@@ -1,8 +1,12 @@
 package com.aezphel.android.twoappassessment;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.actionbar_custom);
+        View actionView = getSupportActionBar().getCustomView();
+        Button addBtn = actionView.findViewById(R.id.addBtn);
+        Button delBtn = actionView.findViewById(R.id.deleteBtn);
+        ImageButton prevBtn = (ImageButton)findViewById(R.id.prevBtn);
+        ImageButton nextBtn = (ImageButton)findViewById(R.id.nextBtn);
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"Add button clicked");
+            }
+        });
+        delBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Delete button clicked");
+            }
+        });
+        prevBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Previous button clicked");
+            }
+        });
+        nextBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Next button clicked");
+            }
+        });
     }
 }
